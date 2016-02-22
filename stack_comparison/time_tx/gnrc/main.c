518e396a165447183f1a3c3185e16934fbc9b90f
Martine Lenders <mail@martine-lenders.eu>
2016-02-22 16:29:35 +0100

    provide timing applications

---
+++ stack_comparison/time_tx/gnrc/main.c
@@ -0,0 +1,20 @@
+/*
+ * Copyright (C) Freie Universität Berlin
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @{
+ *
+ * @file
+ * @author Martine Lenders <mlenders@inf.fu-berlin.de>
+ */
+
+int main(void) {
+    return 0;
+}
+
+/** @} */

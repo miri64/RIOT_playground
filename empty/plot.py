39670a9a141980cfbc4b1c9ecec3bba1ce00c08d
Martine Lenders <mail@martine-lenders.eu>
2016-05-02 17:22:31 +0200

    empty: update plots by measurements without radio sleeping

--- empty/plot.py
+++ empty/plot.py
@@ -10,10 +10,14 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 df_while = pd.read_csv("while_sleepy_radio.oml", skiprows=9, delimiter='\t',
-                       usecols=[5], names=["power while(1)"])
+                       usecols=[5], names=["while(1) (sleep radio)"])
 df_idle = pd.read_csv("idle_sleepy_radio.oml", skiprows=9, delimiter='\t',
-                       usecols=[5], names=["power \"idle\""])
-df = pd.concat([df_while, df_idle], axis=1, join_axes=[df_while.index])
+                       usecols=[5], names=["\"idle\" (sleep radio)"])
+df_while_wo = pd.read_csv("while_wo_radio_driver.oml", skiprows=9, delimiter='\t',
+                       usecols=[5], names=["while(1) (w/o radio)"])
+df_idle_wo = pd.read_csv("idle_wo_radio_driver.oml", skiprows=9, delimiter='\t',
+                       usecols=[5], names=["\"idle\" (w/o radio)"])
+df = pd.concat([df_while, df_idle, df_while_wo, df_idle_wo], axis=1, join_axes=[df_while.index])
 
 df.plot.box(return_type='axes')
 plt.ylabel("Power consumption in W")

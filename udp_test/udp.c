45c0573ef5893e36f255eea25362c00bb26fc5d1
Martine Lenders <mail@martine-lenders.eu>
2016-08-29 12:45:59 +0200

    udp_test: adapt to changes in gnrc_networking

--- udp_test/udp.c
+++ udp_test/udp.c
@@ -131,8 +131,10 @@ static void send(char *addr_str, char *port_str, char *data_len_str, unsigned in
             gnrc_pktbuf_release(ip);
             return;
         }
-        printf("Success: send %u byte to [%s]:%u\n", (unsigned)payload->size,
-               addr_str, port);
+        /* access to `payload` was implicitly given up with the send operation above
+         * => use original variable for output */
+        printf("Success: send %u byte to [%s]:%u\n", (unsigned)data_len, addr_str,
+               port);
         xtimer_usleep(delay);
     }
 }

479eef525731f5f57e1a6a92a75f75ae3bcba9e6
Martine Lenders <m.lenders@fu-berlin.de>
2019-06-12 12:04:37 +0200

    lndw18_luke: coap_service: add more abstraction around OBSERVE

--- coap_service/coap_service.py
+++ coap_service/coap_service.py
@@ -21,6 +21,26 @@ from aiocoap.util import linkformat
 
 logging.basicConfig(level=logging.INFO)
 
+coap_client = None
+
+async def get_coap_client():
+    global coap_client
+
+    if coap_client is None:
+        coap_client = await Context.create_client_context()
+    return coap_client
+
+
+async def shutdown_coap_client():
+    global coap_client
+
+    try:
+        await coap_client.shutdown()
+    except:
+        pass
+    finally:
+        coap_client = None
+
 
 def link_to_json(link):
     return json.dumps(
@@ -28,12 +48,13 @@ def link_to_json(link):
         ).encode("utf-8")
 
 
-async def register(protocol, config_node, target_node):
+async def register(config_node, target_node):
+    coap_client = await get_coap_client()
     request = Message(code=POST, mtype=CON)
     request.set_request_uri(config_node["target"]["href"])
     request.payload = link_to_json(target_node["points"])
     request.opt.content_format = 50 # json
-    pr = protocol.request(request)
+    pr = coap_client.request(request)
     resp = await pr.response
     logging.info("Tried to register {} with {}: {}".format(
             target_node["points"]["href"],
@@ -41,7 +62,7 @@ async def register(protocol, config_node, target_node):
             resp))
 
 
-def incoming_observation(protocol, response):
+def incoming_observation(response):
     links = {}
     nodes = {}
 
@@ -69,39 +90,38 @@ def incoming_observation(protocol, response):
             nodes[node][sense] = link
     if "btn" in nodes and "target" in nodes["btn"] and \
        "dsp" in nodes and "points" in nodes["dsp"]:
-        asyncio.ensure_future(register(protocol, nodes["btn"],
-                                       nodes["dsp"]))
+        asyncio.ensure_future(register(nodes["btn"], nodes["dsp"]))
     if "dsp" in nodes and "target" in nodes["dsp"] and \
        "dino" in nodes and "points" in nodes["dino"]:
-        asyncio.ensure_future(register(protocol, nodes["dsp"],
-                                       nodes["dino"]))
+        asyncio.ensure_future(register(nodes["dsp"], nodes["dino"]))
 
 
-async def main(corerd_addr):
-    protocol = await Context.create_client_context()
 
-    request = Message(code=GET,
-                      uri='coap://[{addr}]/resource-lookup'
-                          .format(addr=corerd_addr),
-                      observe=0)
+async def observe(resource):
+    coap_client = await get_coap_client()
+    request = Message(code=GET, uri=resource, observe=0)
 
     try:
-        pr = protocol.request(request)
+        pr = coap_client.request(request)
         observation_is_over = asyncio.Future()
 
         pr.observation.register_errback(observation_is_over.set_result)
-        pr.observation.register_callback(lambda data, protocol=protocol:
-                incoming_observation(protocol, data)
-            )
-        logging.info("waiting for response")
+        pr.observation.register_callback(incoming_observation)
+        logging.info("waiting for OBSERVE response")
         resp = await pr.response
-        incoming_observation(protocol, resp)
+        incoming_observation(resp)
         exit_reason = await observation_is_over
         logging.info(exit_reason)
     finally:
         if not pr.observation.cancelled:
             pr.observation.cancel()
             await asyncio.sleep(50)
+
+
+async def main(corerd_addr):
+    await observe('coap://[{addr}]/resource-lookup'.format(addr=corerd_addr))
+    # error might have been caused by client so better shut down
+    await shutdown_coap_client()
     # restart main
     event_loop = tornado.ioloop.IOLoop.current()
     event_loop.spawn_callback(main, corerd_addr)

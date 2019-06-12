d26819af6409e2c372b3afdca4547ce9209cf3b6
Martine Lenders <m.lenders@fu-berlin.de>
2019-06-12 14:25:23 +0200

    lndw18_luke: coap_service: add HTTP/CoAP-Request-gateway

--- lndw18_luke/coap_service/coap_service.py
+++ lndw18_luke/coap_service/coap_service.py
@@ -11,11 +11,16 @@ import argparse
 import asyncio
 import json
 import logging
+import socket
+import sys
+from tornado.log import app_log
 import tornado.ioloop
+import tornado.web
 import urllib.parse
 
 from aiocoap import *
-from aiocoap.numbers import media_types
+from aiocoap.message import coap_schemes
+from aiocoap.numbers import media_types, media_types_rev
 from aiocoap.util import linkformat
 
 
@@ -53,7 +58,7 @@ async def register(config_node, target_node):
     request = Message(code=POST, mtype=CON)
     request.set_request_uri(config_node["target"]["href"])
     request.payload = link_to_json(target_node["points"])
-    request.opt.content_format = 50 # json
+    request.opt.content_format = media_types_rev["application/json"]
     pr = coap_client.request(request)
     resp = await pr.response
     logging.info("Tried to register {} with {}: {}".format(
@@ -66,10 +71,12 @@ def incoming_observation(response):
     links = {}
     nodes = {}
 
+    logging.info("Received response {}".format(response))
     if response.code.is_successful():
         cf = response.opt.content_format
         mime_type = media_types.get(cf, "type %s" % cf)
         mime_type, *parameters = mime_type.split(";")
+        logging.info("Content format: {}".format(mime_type))
         if mime_type == "application/link-format":
             ls = linkformat.parse(response.payload.decode('utf-8'))
             for link in ls.links:
@@ -78,6 +85,9 @@ def incoming_observation(response):
                 link["addr"] = pr.netloc
                 link["path"] = pr.path
                 links[link["href"]] = link
+    else:
+        logging.error("")
+        return
     for link in links.values():
         if link["addr"].startswith("[fe80::"):
             logging.error("address {} link-local".format(link["addr"]))
@@ -118,19 +128,76 @@ async def observe(resource):
             await asyncio.sleep(50)
 
 
-async def main(corerd_addr):
+class CoapRequestHandler(tornado.web.RequestHandler):
+    METHODS = {
+        "GET": GET,
+        "POST": POST,
+    }
+
+    async def _common(self):
+        coap_client = await get_coap_client()
+        target = self.get_query_argument("target")
+        p = urllib.parse.urlparse(target)
+        if p.scheme not in coap_schemes:
+            raise tornado.web.HTTPError(400)
+        assert self.request.method in CoapRequestHandler.METHODS
+        request = Message(code=CoapRequestHandler.METHODS[self.request.method],
+                          uri=target)
+        try:
+            if "Content-Type" in self.request.headers:
+                request.opt.content_format = \
+                        media_types_rev[self.request.headers["Content-Type"]]
+        except KeyError:
+            raise tornado.web.HTTPError(400)
+        request.payload = self.request.body
+        pr = coap_client.request(request)
+        app_log.info("waiting for {} response".format(self.request.method))
+        try:
+            resp = await pr.response
+        except socket.gaierror as e:
+            self.log_exception(*sys.exc_info())
+            raise tornado.web.HTTPError(400, str(e))
+        cf = resp.opt.content_format
+        mime_type = media_types.get(cf, "type %s" % cf)
+        mime_type, *parameters = mime_type.split(";")
+        self.content_type = mime_type
+        status_class, status_detail = divmod(resp.code, 0x20)
+        status = ((status_class * 100) + status_detail)
+        if resp.code.is_successful():
+            self.set_status(status, resp.code.name_printable)
+            app_log.info("Success {}, {}".format(resp, resp.payload))
+            self.write(resp.payload)
+        else:
+            app_log.error("Error {}, {}".format(resp, resp.payload))
+            raise tornado.web.HTTPError(status, resp.payload,
+                                        reason=resp.code.name_printable)
+
+
+    async def get(self):
+        return await self._common()
+
+    async def post(self):
+        return await self._common()
+
+
+async def main(corerd_addr, *args, **kwargs):
     await observe('coap://[{addr}]/resource-lookup'.format(addr=corerd_addr))
     # error might have been caused by client so better shut down
     await shutdown_coap_client()
     # restart main
     event_loop = tornado.ioloop.IOLoop.current()
-    event_loop.spawn_callback(main, corerd_addr)
+    event_loop.spawn_callback(main, corerd_addr, *args, **kwargs)
 
 
 if __name__ == "__main__":
     p = argparse.ArgumentParser()
     p.add_argument("corerd_addr")
+    p.add_argument("http_port", type=int, default=5656, nargs="?")
     args = p.parse_args()
+    application = tornado.web.Application([
+        (r"/coap", CoapRequestHandler)
+    ])
+    application.listen(args.http_port)
     event_loop = tornado.ioloop.IOLoop.current()
     event_loop.spawn_callback(main, **vars(args))
     event_loop.start()

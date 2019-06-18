1e901efdb1bdbd51e7aa4154583c0629859b068d
Martine S. Lenders <m.lenders@fu-berlin.de>
2019-06-18 16:12:02 +0200

    lndw18_luke: dino: notify when dino moves

--- dino/main.c
+++ dino/main.c
@@ -32,6 +32,13 @@
 
 #define DINO_PREFIX         "/dino"
 
+static ssize_t _dino_move(coap_pkt_t* pdu, uint8_t *buf, size_t len,
+                          void *ctx);
+
+static const coap_resource_t _resources[] = {
+    { DINO_PREFIX LUKE_PATH_POINTS, COAP_POST | COAP_GET, _dino_move, NULL },
+    { LUKE_PATH_REBOOT, COAP_POST, luke_reboot, NULL },
+};
 static const char corerd_server_addr[] = CORERD_SERVER_ADDR;
 
 static void _clear_dino(void *arg)
@@ -59,42 +66,71 @@ static void _toggle_dino(void *arg)
     xtimer_set(&_clear_timer, 5 * US_PER_SEC);
 }
 
+static void _notify_points(unsigned points)
+{
+    uint8_t buf[GCOAP_PDU_BUF_SIZE];
+    coap_pkt_t pdu;
+
+    /* send Observe notification for /luke/points */
+    switch (gcoap_obs_init(&pdu, &buf[0], GCOAP_PDU_BUF_SIZE,
+            &_resources[0])) {
+        case GCOAP_OBS_INIT_OK: {
+            size_t payload_len = sprintf((char *)pdu.payload, LUKE_POINTS_FMT,
+                                         points);
+            size_t len;
+
+            printf("Sending '%s' (%u bytes) to observers\n",
+                   (char *)pdu.payload, (unsigned)payload_len);
+            len = gcoap_finish(&pdu, payload_len, COAP_FORMAT_JSON);
+            printf("The CoAP message has %u byte\n", (unsigned)len);
+            gcoap_obs_send(&buf[0], len, &_resources[0]);
+            break;
+        }
+        default:
+            break;
+    }
+}
+
 static ssize_t _dino_move(coap_pkt_t* pdu, uint8_t *buf, size_t len,
                           void *ctx)
 {
     unsigned code;
     (void)ctx;
-    if (coap_method2flag(coap_get_code_detail(pdu)) == COAP_POST) {
-        unsigned p = 0;
-        unsigned content_type = coap_get_content_type(pdu);
+    switch (coap_method2flag(coap_get_code_detail(pdu))) {
+        case COAP_GET: {
+            size_t payload_len;
 
-        if ((pdu->payload_len < LUKE_POINTS_MIN_SIZE) ||
-            (pdu->payload_len > LUKE_POINTS_MAX_SIZE) ||
-            (content_type != COAP_FORMAT_JSON) ||
-            (sscanf((char *)pdu->payload, LUKE_POINTS_FMT, &p) != 1)) {
-            printf("(%u < %u) || (%u > %u) || (%u != %u) || "
-                   "(payload unparsable)\n", pdu->payload_len,
-                   LUKE_POINTS_MIN_SIZE, pdu->payload_len, LUKE_POINTS_MAX_SIZE,
-                   content_type, COAP_FORMAT_JSON);
-            code = COAP_CODE_BAD_REQUEST;
+            gcoap_resp_init(pdu, buf, len, COAP_CODE_CONTENT);
+            payload_len = sprintf((char *)pdu->payload, LUKE_POINTS_FMT, 0);
+            return gcoap_finish(pdu, payload_len, COAP_FORMAT_JSON);
         }
-        else {
-            code = COAP_CODE_VALID;
+        case COAP_POST: {
+            unsigned p = 0;
+            unsigned content_type = coap_get_content_type(pdu);
+
+            if ((pdu->payload_len < LUKE_POINTS_MIN_SIZE) ||
+                (pdu->payload_len > LUKE_POINTS_MAX_SIZE) ||
+                (content_type != COAP_FORMAT_JSON) ||
+                (sscanf((char *)pdu->payload, LUKE_POINTS_FMT, &p) != 1)) {
+                printf("(%u < %u) || (%u > %u) || (%u != %u) || "
+                       "(payload unparsable)\n", pdu->payload_len,
+                       LUKE_POINTS_MIN_SIZE, pdu->payload_len, LUKE_POINTS_MAX_SIZE,
+                       content_type, COAP_FORMAT_JSON);
+                code = COAP_CODE_BAD_REQUEST;
+            }
+            else {
+                code = COAP_CODE_VALID;
+            }
+            if (p > 0) {
+                _set_dino(NULL);
+                _notify_points(p);
+            }
+            return gcoap_response(pdu, buf, len, code);
         }
-        if (p > 0) {
-            _set_dino(NULL);
-        }
-        return gcoap_response(pdu, buf, len, code);
-    }
-    else {
-        return 0;
+        default:
+            return 0;
     }
 }
-
-static const coap_resource_t _resources[] = {
-    { DINO_PREFIX LUKE_PATH_POINTS, COAP_POST, _dino_move, NULL },
-    { LUKE_PATH_REBOOT, COAP_POST, luke_reboot, NULL },
-};
 
 static gcoap_listener_t _listener = {
     (coap_resource_t *)&_resources[0],

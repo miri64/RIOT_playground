8bec85729d75da08d8469b3e72e7e8805ae5e61e
Martine Lenders <m.lenders@fu-berlin.de>
2022-06-17 13:30:20 +0200

    lndw22: pythonlibs: increase test coverage

--- lndw22_doc/pythonlibs/tests/test_tornado_jinja2.py
+++ lndw22_doc/pythonlibs/tests/test_tornado_jinja2.py
@@ -5,6 +5,46 @@
 #
 # Distributed under terms of the MIT license.
 
+import os
+
+import jinja2
+import pytest
+import tornado.web
+import tornado.testing
+
+from .. import tornado_jinja2
+
 
 def test_render_template():
-    pass
+    rendering = tornado_jinja2.TemplateRendering()
+    rendering.settings = {
+        "template_path": os.path.join(os.path.dirname(__file__), "tmpl")
+    }
+    res = rendering.render_template("base.html")
+    assert res == "This is the base template"
+    with pytest.raises(jinja2.TemplateNotFound):
+        res = rendering.render_template("foobar.html")
+    rendering.settings["locale_path"] = "foobar"
+    res = rendering.render_template("base.html")
+    del rendering.settings["template_path"]
+    with pytest.raises(jinja2.TemplateNotFound):
+        res = rendering.render_template("base.html")
+
+
+class TestHandler(tornado_jinja2.BaseTemplateHandler):
+    def get(self):
+        return self.render2("base.html")
+
+
+class TestBaseTemplateHandler(tornado.testing.AsyncHTTPTestCase):
+    def get_app(self):
+        app = tornado.web.Application(
+            [
+                (r"/", TestHandler),
+            ]
+        )
+        app.settings["template_path"] = os.path.join(os.path.dirname(__file__), "tmpl")
+        return app
+
+    def test_render2(self):
+        assert self.fetch("/").body == b"This is the base template"
